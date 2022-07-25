313f0bd916661b7f7483c8ef252997df7d16c785
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-07-25 00:14:22 -0300

    :rocket: Projeto finalizado

--- rockethelp/src/routes/index.tsx
+++ rockethelp/src/routes/index.tsx
@@ -2,11 +2,29 @@ import { NavigationContainer } from '@react-navigation/native'
 
 import { SignIn } from '../screens/Signin'
 import { AppRoutes } from './app.routes'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
+import { useState, useEffect } from 'react'
+import { Loading } from '../components/Loading'
 
 export function Routes() {
+    const [loading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(response => {
+            setUser(response);
+            setIsLoading(false);
+        })
+        return subscriber;
+    }, [])
+
+    if(loading) {
+        return  <Loading />
+    }
+
     return (
         <NavigationContainer>
-            <AppRoutes />
+            {user ? <AppRoutes /> : <SignIn />}
         </NavigationContainer>
     )
 }
--- rockethelp/src/screens/Home.tsx
+++ rockethelp/src/screens/Home.tsx
@@ -8,22 +8,19 @@ import { Order, OrderProps } from '../components/Order';
 import { Button } from '../components/Button'
 import { color } from 'native-base/lib/typescript/theme/styled-system';
 import { useNavigation } from '@react-navigation/native'
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
+import React from 'react';
+import { useEffect } from 'react';
+import firestore from '@react-native-firebase/firestore'
+import { dateFormat } from '../utils/firestoreDateFromat';
+import { isLoading } from 'expo-font';
+import { Loading } from '../components/Loading';
+
 export function Home() {
+    const [isLoading, setIsLoading] = useState(true)
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-    const [orders, setOders] = useState<OrderProps[]>([
-        {
-            id: '123',
-            patrimony: '123456',
-            when: '18/07/2022 as 14:00',
-            status: 'open'
-        },
-        {
-            id: '312',
-            patrimony: '312',
-            when: '18/07/2022 as 14:00',
-            status: 'closed'
-        }
-    ]);
+    const [orders, setOders] = useState<OrderProps[]>([]);
 
     const { colors } = useTheme();
     const navigation = useNavigation()
@@ -35,6 +32,40 @@ export function Home() {
     function handleOpenDetails(orderId: string) {
         navigation.navigate('details', { orderId })
     }
+
+    function handleLogout() {
+        auth()
+            .signOut()
+            .catch(
+                error => {
+                    console.log(error);
+                    return Alert.alert('Sair', 'Não foi possível sair');
+                }
+            )
+    }
+
+    useEffect(() => {
+        setIsLoading(true)
+
+        const subscriber = firestore()
+            .collection('orders')
+            .where('status', '==', statusSelected)
+            .onSnapshot(snapshot => {
+                const data = snapshot.docs.map(doc => {
+                    const { patrimony, description, status, created_at } = doc.data();
+                    return {
+                        id: doc.id,
+                        patrimony,
+                        description,
+                        status,
+                        when: dateFormat(created_at)
+                    }
+                })
+                setOders(data);
+                setIsLoading(false);
+            })
+        return subscriber
+    }, [statusSelected])
 
     return (
         <VStack flex={1} pb={6} bg="gray.700">
@@ -51,6 +82,7 @@ export function Home() {
                 <Logo />
                 <IconButton
                     icon={<SignOut size={26} color={colors.gray[300]} />}
+                    onPress={handleLogout}
                 />
             </HStack>
             <VStack flex={1} px={6}>
@@ -78,7 +110,9 @@ export function Home() {
                     />
                 </HStack>
 
-                <FlatList
+                {
+                    isLoading ? <Loading /> :
+                    <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
                     renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
@@ -95,6 +129,7 @@ export function Home() {
                     )}
                 >
                 </FlatList>
+                }
                 <Button title="Nova solicitação" onPress={handleNewOrder}></Button>
             </VStack>
         </VStack>
